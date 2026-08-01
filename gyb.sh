3c0b861f0d81160b24a2c369d3ee5a3c7a7203f7
GruiaChiscop <166289528+GruiaChiscop@users.noreply.github.com>
2026-08-01 12:17:09 +0300

    Fix app distribution packaging

--- gyb.sh
+++ gyb.sh
@@ -1,8 +1,3 @@
-cd BetterTG/Secret
-find . -name "*.gyb" |
-while read file; do
-    filename=$(echo "$file" | sed 's/.\///')
-    API_ID=$1 API_HASH=$2 gyb --line-directive '' -o "${filename%.gyb}" "$filename";
-done
-
-cd ../..
+API_ID="$1" API_HASH="$2" gyb --line-directive '' \
+    -o BetterTG/Secret/Secret.swift \
+    templates/Secret.swift.gyb
